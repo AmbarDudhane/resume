aefc47ab9262627c61281ad3ed76fd2a1a9f7062
Ambar Dudhane <ambarsd12345@hotmail.com>
2021-04-06 22:26:53 -0500

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from "./logo.svg";
+import ReactGA from "react-ga";
 
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Main from "./components/main";
 import Footer from "./components/footer";
+
+const TRACKING_ID = "UA-194073116-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
